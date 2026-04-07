c34bc19d7ea232ac6ddd2e5e3b583cc6aa9569f3
Jyothi Prakash Chinni <118927425+SirxChinni@users.noreply.github.com>
2026-04-07 22:03:17 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'https://job-hunt-frontend-6fl2.onrender.com',
     credentials: true,
 }
 app.use(cors(corsOptions));
